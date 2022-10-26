13e184548a9aa592bbdd8975605c7b5ae2f854a0
fghinzelli <fghinzelli@gmail.com>
2022-10-26 07:27:41 -0300

    New form

--- reactjs/react-complete-guide/src/App.js
+++ reactjs/react-complete-guide/src/App.js
@@ -1,15 +1,19 @@
-
-import Expenses from './components/Expenses/Expenses';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpese";
 
 const App = () => {
-
   const items = [
-    { title: "Market", amount: 500, date: new Date(2022, 5, 1)},
-    { title: "Fuel", amount: 100, date: new Date(2022, 7, 2)},
-    { title: "Energy", amount: 200, date: new Date(2022, 8, 3)},
+    { title: "Market", amount: 500, date: new Date(2022, 5, 1) },
+    { title: "Fuel", amount: 100, date: new Date(2022, 7, 2) },
+    { title: "Energy", amount: 200, date: new Date(2022, 8, 3) },
   ];
 
-  return (<Expenses items={items} />)
-}
+  return (
+    <>
+      <NewExpense />
+      <Expenses items={items} />
+    </>
+  );
+};
 
 export default App;
