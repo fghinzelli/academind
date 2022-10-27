dc1fe279f9829aa13947ee9b330c7b1350c8e1e0
Fernando Ghinzelli <fghinzelli@gmail.com>
2022-10-27 13:02:22 -0300

    Form submit with state.

--- reactjs/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ reactjs/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -4,22 +4,22 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    
-  const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle('Updated!');
-  }
+    const [title, setTitle] = useState(props.title);
 
-  return (
-    <Card className="expense-item">
-        <ExpenseDate date={props.date} />
-        <div className="expense-item__description">
-            <h2>{ title }</h2>
-            <div className="expense-item__price">{ props.amount }</div>
-            <button onClick={clickHandler}>Change title</button>
-        </div>
-    </Card>
+    const clickHandler = () => {
+        setTitle('Updated!');
+    }
+
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
+                <button onClick={clickHandler}>Change title</button>
+            </div>
+        </Card>
     )
 }
 
--- reactjs/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ reactjs/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  // Example with only one state
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // });
+
+  const titleChangeHandler = event => {
+
+    // Example with update one state. Use function to get previous state
+    // setUserInput(prevState => {
+    //   return { ...prevState, enteredTitle: event.target.value }
+    // })
+
+    setEnteredTitle(event.target.value);
+  }
+
+  const amountChangeHandler = event => {
+    setEnteredAmount(event.target.value);
+  }
+
+  const dateChangeHandler = event => {
+    setEnteredDate(event.target.value);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+    console.log(enteredData);
+  }
+
   return (
-    <form action="">
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-01" />
+          <input type="date" min="2019-01-01" max="2022-12-01" onChange={dateChangeHandler} />
         </div>
       </div>
+      <div className="new-expense__actions">
+        <button>Add Expense</button>
+      </div>
     </form>
+
   );
 };
 
