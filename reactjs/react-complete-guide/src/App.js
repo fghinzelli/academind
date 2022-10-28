64c32bf3a2bfd404a4a0ff53dde766d65ccdd5c5
Fernando Ghinzelli <fghinzelli@gmail.com>
2022-10-28 14:06:30 -0300

    Challenges and exercises

--- reactjs/react-complete-guide/src/App.js
+++ reactjs/react-complete-guide/src/App.js
@@ -1,22 +1,27 @@
+
+import React from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpese";
 
 const App = () => {
+  
   const items = [
     { title: "Market", amount: 500, date: new Date(2022, 5, 1) },
     { title: "Fuel", amount: 100, date: new Date(2022, 7, 2) },
     { title: "Energy", amount: 200, date: new Date(2022, 8, 3) },
   ];
 
+
   const addExpense = (newExpense) => {
     console.log('On App.js')
     console.log(newExpense)
   }
 
+
   return (
     <>
       <NewExpense onAddExpense={addExpense} />
-      <Expenses items={items} />
+      <Expenses items={items} s/>
     </>
   );
 };
--- reactjs/react-complete-guide/src/components/Expenses/Expenses.js
+++ reactjs/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,17 +1,27 @@
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [selectedYear, setSelectedYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        console.log('selectedYear', selectedYear)
+        setSelectedYear(selectedYear)
+    }
+
     return (
-        <Card className="expenses">
-            {props.items.map(item => <ExpenseItem 
-                key={item.title} 
-                title={item.title} 
-                amount={item.amount} 
-                date={item.date}/>
-            )}
-        </Card>
+            <Card className="expenses">
+                <ExpensesFilter selected={selectedYear} onFilterChange={filterChangeHandler} />
+                {props.items.map(item => <ExpenseItem
+                    key={item.title}
+                    title={item.title}
+                    amount={item.amount}
+                    date={item.date} />
+                )}
+            </Card>
     );
 }
 
