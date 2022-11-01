88ed9bdeed690a0ed416a5dcb7beedd69d1eb22a
fghinzelli <fghinzelli@gmail.com>
2022-11-01 06:54:12 -0300

    Conditional render

--- reactjs/react-complete-guide/src/components/Expenses/Expenses.js
+++ reactjs/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,31 +1,40 @@
-import React, { useState } from 'react';
-import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-    const [selectedYear, setSelectedYear] = useState('2020');
+  const [selectedYear, setSelectedYear] = useState("2020");
 
-    const filterChangeHandler = selectedYear => {
-        setSelectedYear(selectedYear);
-    }
+  const filterChangeHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+  };
 
-    const filteredItems = props.items.filter(item => {
-        return item.date.getFullYear().toString() === selectedYear
-    })
+  const filteredItems = props.items.filter((item) => {
+    return item.date.getFullYear().toString() === selectedYear;
+  });
 
-    return (
-            <Card className="expenses">
-                <ExpensesFilter selected={selectedYear} onFilterChange={filterChangeHandler} />
-                {filteredItems.map(item => <ExpenseItem
-                    key={item.title}
-                    title={item.title}
-                    amount={item.amount}
-                    date={item.date} />
-                )}
-            </Card>
-    );
-}
+  let listItems = <p>No items found.</p>
+
+  if (filteredItems.length > 0) {
+      listItems = filteredItems.map(item => <ExpenseItem
+          key={item.title}
+          title={item.title}
+          amount={item.amount}
+          date={item.date} />
+      )
+  }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={selectedYear}
+        onFilterChange={filterChangeHandler}
+      />
+      { listItems }
+    </Card>
+  );
+};
 
 export default Expenses;
