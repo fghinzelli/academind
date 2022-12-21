86a8384d4f27011838282860884aabef19b4a67e
fghinzelli <fghinzelli@gmail.com>
2022-12-21 07:10:07 -0300

    Show Overlay with ReactDOM Portal

--- reactjs/time-to-practice1/src/components/Popup/Popup.js
+++ reactjs/time-to-practice1/src/components/Popup/Popup.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import style from './Popup.module.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import style from "./Popup.module.css";
+
+const OverlayRoot = (props) => {
+  return (
+    <div onClick={props.closePopup} className={style.PopupBackground}>
+      <div className={style.PopupWrapper}>
+        <div className={style.PopupHeader}>Invalid input</div>
+        <div className={style.PopupBody}>
+          <p>{props.error}</p>
+        </div>
+        <div className={style.PopupFooter}>
+          <button className={style.FooterButton} onClick={props.closePopup}>
+            OK
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Popup = (props) => {
-    return (
-        <div onClick={props.closePopup} className={style.PopupBackground}>
-            <div className={style.PopupWrapper}>
-                <div className={style.PopupHeader}>Invalid input</div>
-                <div className={style.PopupBody}>
-                    <p>{props.error}</p>
-                </div>
-                <div className={style.PopupFooter}>
-                    <button className={style.FooterButton} onClick={props.closePopup}>OK</button>
-                </div>
-            </div>
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <OverlayRoot 
+            closePopup={props.closePopup}
+            error={props.error}
 
-        </div>
-    );
-}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </>
+  );
+};
 
 export default Popup;
